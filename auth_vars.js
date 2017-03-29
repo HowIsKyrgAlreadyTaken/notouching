c730dad0cea60993c53d655bb2c9b236a3056c21
HowIsKyrgAlreadyTaken <thorawayfun@gmail.com>
2017-03-29 21:20:37 +0200

    Delete auth_vars.js

--- auth_vars.js
+++
@@ -1,9 +0,0 @@
-//no worries
-
-
-module.exports = {
-    consumer_key: '7YNuiqqwjw2PNrLOQUjeYNkVw' ,
-    consumer_secret: 'w4J1Sxq7BsodG7hDNh18OA0zpDdEcbYlcPlmJwYLtT9CJPhgOY' ,
-    access_token_key: 'suddenly_here' ,
-    access_token_secret: '789589028425302016'
-}
