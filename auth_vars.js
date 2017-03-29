5ecbceeffe0e5b47b45624734e7ff80e262352d6
Your Name <you@example.com>
2017-03-29 21:16:24 +0200

    changing the authsVars

--- auth_vars.js
+++ auth_vars.js
@@ -1,6 +1,9 @@
+//no worries
+
+
 module.exports = {
-    consumer_key: 'MkmvSAo7qDHAhLN82LnChDRzr' ,
-    consumer_secret: 'KcjgNuuRgMeaXxUEFpBNPOqvHPyFRon19YAcB0jtEyO8GY2kpJ' ,
-    access_token_key: 'suddenly_here' ,
-    access_token_secret: '789589028425302016'
+    consumer_key: 'I' ,
+    consumer_secret: 'rerolled' ,
+    access_token_key: 'anyway' ,
+    access_token_secret: 'k'
 }
