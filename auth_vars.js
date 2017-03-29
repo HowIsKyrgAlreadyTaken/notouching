24d2ac18147bd10102b0d94929b38554fedca7b6
Your Name <you@example.com>
2017-03-29 21:14:12 +0200

    making git ignore node_modules and adding some file

---
+++ auth_vars.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'MkmvSAo7qDHAhLN82LnChDRzr' ,
+    consumer_secret: 'KcjgNuuRgMeaXxUEFpBNPOqvHPyFRon19YAcB0jtEyO8GY2kpJ' ,
+    access_token_key: 'suddenly_here' ,
+    access_token_secret: '789589028425302016'
+}
